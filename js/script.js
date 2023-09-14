df39949df397b0a2e8e96f344ed5b2d63ac32e27
joaness01 <joanonyinye480>
2023-09-14 23:56:25 +0100

    age

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+let num1=365;
+let num2 = 52;
+let num3 = 12 ;
+let num4= 90;
+let btnSubmit = document.getElementById("btnSubmit");
+let result = document.getElementById("result");
+let age = document.getElementById("age");
+
+
+btnSubmit.addEventListener("click", display);
+
+ function display(){
+    let age =parseInt(document.getElementById("age").value);
+    let year = num4 - age;
+    let days = year * num1;
+    let weeks = year * num2;
+    let month = year * num3;
+    result.textContent= "Total Days: " + days +" Total Weeks: " + weeks + " Total Months:" + month;
+
+}
+
+
+
+
+
