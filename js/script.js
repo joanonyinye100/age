f1708cd07ace775c43dd12e71d3b45c6e96242af
joaness01 <joanonyinye480>
2023-09-15 14:08:15 +0100

    age

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-let num1=365;
-let num2 = 52;
-let num3 = 12 ;
-let num4= 90;
+const numOfDays=365;
+const numOfWeeks = 52;
+const numOfMonth = 12 ;
+const expectedLifeSpan= 90;
 let btnSubmit = document.getElementById("btnSubmit");
 let result = document.getElementById("result");
 let age = document.getElementById("age");
@@ -12,11 +12,16 @@ btnSubmit.addEventListener("click", display);
 
  function display(){
     let age =parseInt(document.getElementById("age").value);
-    let year = num4 - age;
-    let days = year * num1;
-    let weeks = year * num2;
-    let month = year * num3;
+    let expectedLifeCycle = expectedLifeSpan - age;
+    let days = expectedLifeCycle * numOfDays;
+    let weeks = expectedLifeCycle * numOfWeeks;
+    let month = expectedLifeCycle * numOfWeeks;
     result.textContent= "You have " + days + " Days, " + weeks + " Weeks, "  + month  + " Months left.";
+    if(age > 90){
+        alert("Please enter correct Age");
+    } else {
+        result.textContent= "You have " + days + " Days, " + weeks + " Weeks, "  + month  + " Months left.";
+    }
 
 }
 
